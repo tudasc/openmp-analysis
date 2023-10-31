f6421ab6c7e2ca2ef239311660e5ae37f221efe3
Gerhard, Louis <louis.gerhard@stud.tu-darmstadt.de>
2023-10-31 17:46:36 +0100

    install.sh update

--- install.sh
+++ install.sh
@@ -7,7 +7,4 @@ sudo apt-get install python3-venv
 python3 -m venv ./venv/openmp-usage-analysis
 source ./venv/openmp-usage-analysis/bin/activate
 pip install -r requirements.txt
-
-chmod +x ./openmp_usage_analysis_search.sh
-chmod +x ./openmp_usage_analysis_analyze.sh
-chmod +x ./openmp_usage_analysis_evaluate.sh
+source ./venv/openmp-usage-analysis/bin/activate
