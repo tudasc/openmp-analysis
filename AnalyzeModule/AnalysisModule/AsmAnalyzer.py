6d85e31910d00074fcd67342b507445f5b2c63e4
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-02-23 11:31:07 +0100

    WIP: detection if cmp is based on thread num

--- AnalyzeModule/AnalysisModule/AsmAnalyzer.py
+++ AnalyzeModule/AnalysisModule/AsmAnalyzer.py
@@ -7,7 +7,6 @@ import subprocess
 from collections import OrderedDict
 import matplotlib.pyplot as plt
 
-
 import re
 
 import angr
@@ -42,6 +41,12 @@ def get_pruned_cfg(graph_in):
     for edge in to_remove:
         graph.remove_edge(edge[0], edge[1])
     return graph
+
+
+def is_register(reg):
+    return reg in ['rax', 'rbx', 'rcx', 'rsp', 'rbp', 'rdi', 'rsi', 'rdx',
+                   'eax', 'ebx', 'ecx', 'esp', 'ebp', 'edi', 'esi', 'edx',
+                   'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15']
 
 
 class OpenMPRegionAnalysis(angr.Analysis):
@@ -102,7 +107,114 @@ class OpenMPRegionAnalysis(angr.Analysis):
         # not found
         return None
 
-    def handleLoop(self, loop, this_function_cfg, entry_node, region):
+    # returns set of addresses of each instruction in the function that we know to be based on thread_num
+    def get_instructions_based_on_thread_num(self, REMOVE_PARAM, this_function_loop_free_cfg, entry_node):
+        return_register = 'eax'  # may be architecture specific
+        try:
+            thread_num_func = self.kb.functions['omp_get_thread_num']
+        except KeyError:
+            return set()  # empty
+
+        result = set()
+
+        to_visit = dict()
+        to_add = dict()  # BFS
+        visited = set()
+        # find calls to omp_get_thread_num
+        for bb_addr in this_function_loop_free_cfg:
+            for tgt, jmp_kind in self.cfg.get_successors_and_jumpkind(bb_addr):
+                if jmp_kind == 'Ijk_Call':
+                    tgt_func = self.kb.functions.get_by_addr(tgt.addr)
+                    if tgt_func == thread_num_func:
+                        bb = self.project.factory.block(bb_addr.addr)
+                        terminator = bb.disassembly.insns[-1]
+                        assert terminator.mnemonic == "call"
+                        print(terminator)
+                        result.add(terminator)
+                        # move from there
+                        tainted_registers = {return_register}
+                        ret_blocks = list(this_function_loop_free_cfg.neighbors(bb_addr))
+                        assert len(ret_blocks) == 1
+                        ret_block = ret_blocks[0]
+                        assert ret_block not in to_visit
+                        to_visit[ret_block] = tainted_registers.copy()
+                        visited.add(bb_addr)
+
+        while not len(to_visit) == 0 or not len(to_add) == 0:
+            if len(to_visit) == 0:
+                to_visit = to_add.copy()
+                to_add.clear()
+
+            bb_addr = list(to_visit.keys())[0]
+            tainted_registers = to_visit[bb_addr]
+            to_visit.pop(bb_addr)
+            # only if all incoming edges are visited
+            incoming_visited = True
+            for inc in this_function_loop_free_cfg.predecessors(bb_addr):
+                if inc not in visited:
+                    incoming_visited = False
+                    break
+            if not incoming_visited:
+                if not len(to_visit) == 0 and len(to_add) == 0:  # avoid endless recursion
+                    # TODO there exist other possibilities of endless recursion
+                    # to be visitied later
+                    if bb_addr in to_add:
+                        to_add[bb_addr] = tainted_registers.intersection(to_add[bb_addr])
+                    else:
+                        to_add[bb_addr] = tainted_registers.copy()
+                    continue
+            bb = self.project.factory.block(bb_addr.addr)
+            #TODO debugg here with debugger
+            for inst in bb.disassembly.insns:
+                operands = inst.op_str.split(',')
+                if len(operands) == 2:
+                    if operands[0].strip() in tainted_registers:
+                        if not inst.mnemonic == "cmp":
+                            # register overwritten
+                            # this is the conservative method anything written into this register marks it as not dependent on thread num anymore
+                            # if e.g. another dependant value gets moved here, it could still be tainted
+                            # but this requires more logic to e.g. distinguish it from "xor eax,eax" here the result is not dependant anymore
+                            tainted_registers.remove(operands[0].strip())
+
+                    if operands[1].strip() in tainted_registers:
+                        print(inst)
+                        result.add(inst)
+                        if is_register(operands[0].strip()):
+                            tainted_registers.add(operands[0].strip())
+
+                elif len(operands) == 1:
+                    if inst.mnemonic == 'call':
+                        if return_register in tainted_registers:
+                            tainted_registers.remove(return_register)
+                    if operands[0].strip() in tainted_registers:
+                        tainted_registers.remove(operands[0].strip())
+                        # remove, may be written (e.g. pop)
+                else:
+                    print(inst)
+                    if inst.mnemonic == "ret":
+                        continue  # end of this branch
+                    elif inst.mnemonic == "cdq":
+                        if 'edx' in tainted_registers:
+                            # overwritten
+                            tainted_registers.remove('edx')
+                    else:
+                        assert False and "operation not supported"
+
+            # end for insts
+
+            for succ in this_function_loop_free_cfg.successors(bb_addr):
+                if succ in to_visit:
+                    to_visit[succ] = tainted_registers.intersection(to_visit[succ])
+                elif succ in to_add:
+                    to_add[succ] = tainted_registers.intersection(to_add[succ])
+                else:
+                    if len(tainted_registers) > 0:  # abort early if nothing more to do
+                        to_add[succ] = tainted_registers.copy()
+
+        assert False
+        return result
+
+    def handleLoop(self, loop, this_function_cfg, this_function_loop_free_cfg, entry_node, region):
         # try to get trip count of loop
 
         trip_count_guess = 'DEFAULT'
@@ -118,15 +230,39 @@ class OpenMPRegionAnalysis(angr.Analysis):
                     print(type(cmp))
                     # found the loops cmp instruction
                     # check if it has a constant value
+                    operand_1 = cmp.op_str.split(',')[0].strip()
                     operand_2 = cmp.op_str.split(',')[1].strip()
                     print(operand_2)
+                    as_int = None
+                    try:
+                        as_int = int(operand_2)  # decimal constant
+
+                    except ValueError:
+                        pass
                     if hex_pattern.match(operand_2):  # is hexnum
                         as_int = int(operand_2[2:], 16)
+                    if as_int is not None:
                         print("Found Constant Trip count of loop: %d" % int(as_int))
-                        trip_count_guess = int(as_int)
-                        #TODO add check that other cmp part is incremented by 1 each loop trip
+                        # check if other opreand is register and incremented by 1 every loop trip
+                        if is_register(operand_1):
+                            for bb_addr in loop:
+                                bb = self.project.factory.block(bb_addr.addr)
+                                for inst in bb.disassembly.insns:
+                                    if inst.mnemonic == 'add':
+                                        print(inst.op_str.split(',')[0].strip())
+                                        if inst.op_str.split(',')[0].strip() == operand_1:
+                                            if inst.op_str.split(',')[1].strip() == '1':
+                                                # found increment by 1
+                                                trip_count_guess = int(as_int)
+                                                break
+                    # end if as_int not None
 
-                    # TODO check if val is known to be based of num_threads
+                    # check if val is known to be based of num_threads
+                    # TODO optimization: dont calculate the set several times for multiple loops
+                    if cmp in self.get_instructions_based_on_thread_num(this_function_cfg, this_function_loop_free_cfg,
+                                                                        entry_node):
+                        assert trip_count_guess == 'DEFAULT'
+                        trip_count_guess = 'DEPEND_ON_THREAD_NUM'
                     # pass
 
         region.loops += 1
@@ -225,7 +361,8 @@ class OpenMPRegionAnalysis(angr.Analysis):
             # self.loops contain all loops from all functions
             # we only handle loops in current function:
             if loop[0] in this_function_cfg.nodes:
-                loop_trip_count_factor = self.handleLoop(loop, this_function_cfg, function_entry_cfg_node,
+                loop_trip_count_factor = self.handleLoop(loop, this_function_cfg, loop_free_cfg,
+                                                         function_entry_cfg_node,
                                                          current_region)
                 for block in loop:
                     block_weights[block] *= loop_trip_count_factor
@@ -240,13 +377,12 @@ class OpenMPRegionAnalysis(angr.Analysis):
 
             # successors
             for tgt, jmp_kind in self.cfg.get_successors_and_jumpkind(cfg_node):
-                # TODO handle if
                 # handle call
                 if jmp_kind == 'Ijk_Call':
                     tgt_func = self.kb.functions.get_by_addr(tgt.addr)
                     if not tgt_func == func:
                         target_call_region = self.analyze_function(tgt_func)
-                        # TODO also use block weight
+                        # TODO also use block weight?
                         current_region.include_other(target_call_region)
                     else:
                         # simple recursion
