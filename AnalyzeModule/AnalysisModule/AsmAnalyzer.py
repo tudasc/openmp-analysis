8cb431461705326660ab2603fd3e715af4c9aaee
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-02-29 15:30:31 +0100

    ad handling of general registers

--- AnalyzeModule/AnalysisModule/AsmAnalyzer.py
+++ AnalyzeModule/AnalysisModule/AsmAnalyzer.py
@@ -46,12 +46,17 @@ def get_pruned_cfg(graph_in):
 def is_register(reg):
     return reg in ['rax', 'rbx', 'rcx', 'rsp', 'rbp', 'rdi', 'rsi', 'rdx',
                    'eax', 'ebx', 'ecx', 'esp', 'ebp', 'edi', 'esi', 'edx',
-                   'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15']
+                   'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15',
+                   'r8d', 'r9d', 'r10d', 'r11d', 'r12d', 'r13d', 'r14d', 'r15d',
+                   'r8w', 'r9w', 'r10w', 'r11w', 'r12w', 'r13w', 'r14w', 'r15w']
 
 
 # if one reg is only a lower part of another
 register_equivalent = [['rax', 'eax', 'ax'], ['rbx', 'ebx', 'bx'], ['rcx', 'ecx', 'cx'], ['rdx', 'edx', 'dx'],
-                       ['rsp', 'esp', 'sp'], ['rsb', 'esb', 'sb'], ['rdi', 'edi', 'di'], ['rsi', 'esi', 'si']]
+                       ['rsp', 'esp', 'sp'], ['rsb', 'esb', 'sb'], ['rdi', 'edi', 'di'], ['rsi', 'esi', 'si'],
+                       ['r8', 'r8d', 'r8w'], ['r9', 'r9d', 'r9w'], ['r10', 'r10d', 'r10w'], ['r11', 'r11d', 'r11w'],
+                       ['r12', 'r12d', 'r12w'], ['r13', 'r13d', 'r13w'], ['r14', 'r14d', 'r14w'],
+                       ['r15', 'r15d', 'r15w']]
 
 
 def add_tainted_register(set, reg):
