50db5f990aecbd2f7015858792162512a826be82
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-02-29 16:47:31 +0100

    fix openmp sample code

--- REPOS_SAMPLE/04/openmp_example.c
+++ REPOS_SAMPLE/04/openmp_example.c
@@ -5,7 +5,7 @@
 int main(int argc, char **argv) {
 #pragma omp parallel for
   for (int i = 0; i < 100; ++i) {
-    for (int j = 0; i < 100; ++i) {
+    for (int j = 0; j < 100; ++j) {
       printf("%d\n", i + j);
     }
   }
