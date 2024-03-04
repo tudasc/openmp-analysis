1deba373bb27c7a253c72ee3137012ab76d02bf3
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-03-04 14:43:31 +0100

    Refactoring: moved code

--- AnalyzeModule/AnalysisModule/AsmAnalyzer.py
+++ AnalyzeModule/AnalysisModule/AsmAnalyzer.py
@@ -1,21 +1,12 @@
-import itertools
-import os
-import sys
-import shutil
-import math
-import subprocess
-from collections import OrderedDict
-import matplotlib.pyplot as plt
-
 import re
 
 import angr
 import networkx
 import networkx as nx
 import pandas as pd
-import numpy as np
 from angrutils import plot_cfg
-from networkx import NetworkXError
+
+from AnalyzeModule.AnalysisModule.CFGAnalysis import get_loop_guard, get_block_weight, remove_back_edges
 
 hex_pattern = re.compile("0[xX][0-9a-fA-F]+")
 
@@ -86,128 +77,6 @@ def remove_tainted_register(set, reg):
                 if rr in set:
                     set.remove(rr)
 
-def dominates( u, v, im_dominators):
-    prev_node = v
-    cur_node = im_dominators[v]
-    while prev_node != cur_node:
-        if cur_node == u:
-            return True  # found u in domtree
-        # go one lvl up in the domtree
-        prev_node = cur_node
-        cur_node = im_dominators[prev_node]
-
-    return False
-
-def get_loop_guard(loop, this_function_cfg, entry_node):
-    # the loop guard block dominates all loop blocks
-    im_dominators = networkx.immediate_dominators(this_function_cfg, entry_node)
-
-    guards = []
-    for candidate in loop:
-        is_entry = True
-        is_exit = True
-
-        for bbb in loop:
-            if dominates(candidate, bbb, im_dominators):
-                is_entry = False
-            if dominates(bbb, candidate, im_dominators):
-                is_exit = False
-        if is_entry or is_exit:
-            guards.append(candidate)
-
-    leave_loop = []
-    for guard in guards:
-        goes_outside = False
-        for succ in this_function_cfg.successors(guard):
-            if succ not in loop:
-                goes_outside = True
-                break
-        if goes_outside:
-            leave_loop.append(guard)
-
-    if len(leave_loop) == 1:
-        return leave_loop[0]
-
-    # not found
-    return None
-
-# calculate weight of each block (probability of execution ignoring loops)
-# with each branch having equal probability
-def get_block_weight(loop_free_cfg, entry_node):
-    assert len(list(nx.simple_cycles(loop_free_cfg))) == 0  # no cycles
-    result = {node: 0.0 for node in loop_free_cfg.nodes}
-    result[entry_node] = 1.0
-
-    to_visit = {entry_node}
-    to_add = set()  # to implement BFS
-    visited = set()
-
-    while len(to_visit) > 0:
-        node = to_visit.pop()
-        for pred in loop_free_cfg.predecessors(node):
-            if pred not in visited:
-                # not all incoming edges where visited
-                to_add.add(node)  # need to re-visit in next BFS wave
-                continue
-        visited.add(node)
-        if node in to_add:
-            to_add.remove(node)  # no endless recursion
-        num_successors = len(loop_free_cfg.succ[node])
-        for succ in loop_free_cfg.succ[node]:
-            to_add.add(succ)
-            # propagate probability
-            result[succ] += result[node] * (1.0 / num_successors)
-
-        if len(to_visit) == 0:
-            to_visit = to_add.copy()
-            to_add.clear()
-
-    return result
-
-# remove all loop back edges from cfg
-# iterative algorithm: remove the edge that is included in most loops, but only if all nodes are still reachable, until no more loops remain
-def remove_back_edges(this_function_cfg, entry_node):
-    result = this_function_cfg.copy()
-    reachable = {entry_node} | networkx.descendants(result,
-                                                    entry_node)
-    assert reachable == set(result.nodes)
-
-    loops = list(networkx.simple_cycles(result))
-    num_loops = len(loops)
-    while len(loops) > 0:
-        removal_candidates = {}
-        # collect removal candidates
-        for loop in loops:
-            for i in range(len(loop) - 1):
-                if (loop[i], loop[i + 1]) not in removal_candidates:
-                    removal_candidates[(loop[i], loop[i + 1])] = 0
-                removal_candidates[(loop[i], loop[i + 1])] += 1
-            # wrap-around
-            if (loop[-1], loop[0]) not in removal_candidates:
-                removal_candidates[(loop[-1], loop[0])] = 0
-            removal_candidates[(loop[-1], loop[0])] += 1
-        # sort by number of cycles the edge is part of
-        removal_candidates = dict(sorted(removal_candidates.items(), key=lambda item: item[1]))
-        # try removal
-        for edge, _ in removal_candidates.items():
-            result.remove_edge(edge[0], edge[1])
-            reachable = {entry_node} | networkx.descendants(result,
-                                                            entry_node)
-            if not reachable == set(result.nodes):
-                # removal partitioned graph
-                result.add_edge(edge[0], edge[1])
-            else:
-                # found edge to remove
-                break
-        # check for other loops
-        loops = list(networkx.simple_cycles(result))
-        assert len(loops) < num_loops
-        num_loops = len(loops)
-
-    reachable = {entry_node} | networkx.descendants(result, entry_node)
-    assert reachable == set(result.nodes)
-    assert len(list(networkx.simple_cycles(result))) == 0
-    return result
 
 class OpenMPRegionAnalysis(angr.Analysis):
 
@@ -231,8 +100,6 @@ class OpenMPRegionAnalysis(angr.Analysis):
 
     def handleRecursion(self, region):
         region['recursions'] += 1
-
-
 
     # returns set of addresses of each instruction in the function that we know to be based on thread_num
     def get_instructions_based_on_thread_num(self, this_function_loop_free_cfg):
@@ -446,10 +313,6 @@ class OpenMPRegionAnalysis(angr.Analysis):
             region['known_tripcount_loops'] += 1
         return trip_count_guess
 
-
-
-
-
     def analyze_function(self, func):
         if func in self.function_analysis_result_cache:
             return self.function_analysis_result_cache[func]
@@ -494,8 +357,8 @@ class OpenMPRegionAnalysis(angr.Analysis):
                     tgt_func = self.kb.functions.get_by_addr(tgt.addr)
                     if not tgt_func == func:
                         target_call_region = self.analyze_function(tgt_func)
-                        #TODO use block weight?
-                        combine_region(current_region,target_call_region)
+                        # TODO use block weight?
+                        combine_region(current_region, target_call_region)
                     else:
                         # simple recursion
                         # nothing to do, recursion is handled later
@@ -521,6 +384,7 @@ class OpenMPRegionAnalysis(angr.Analysis):
 
 angr.analyses.register_analysis(OpenMPRegionAnalysis,
                                 'OpenMPRegionAnalysis')  # register the class with angr's global analysis list
+
 
 class AsmAnalyzer:
     __slots__ = ()
---
+++ AnalyzeModule/AnalysisModule/CFGAnalysis.py
@@ -0,0 +1,129 @@
+import networkx
+import networkx as nx
+
+
+def dominates(u, v, im_dominators):
+    prev_node = v
+    cur_node = im_dominators[v]
+    while prev_node != cur_node:
+        if cur_node == u:
+            return True  # found u in domtree
+        # go one lvl up in the domtree
+        prev_node = cur_node
+        cur_node = im_dominators[prev_node]
+
+    return False
+
+
+def get_loop_guard(loop, this_function_cfg, entry_node):
+    # the loop guard block dominates all loop blocks
+    im_dominators = networkx.immediate_dominators(this_function_cfg, entry_node)
+
+    guards = []
+    for candidate in loop:
+        is_entry = True
+        is_exit = True
+
+        for bbb in loop:
+            if dominates(candidate, bbb, im_dominators):
+                is_entry = False
+            if dominates(bbb, candidate, im_dominators):
+                is_exit = False
+        if is_entry or is_exit:
+            guards.append(candidate)
+
+    leave_loop = []
+    for guard in guards:
+        goes_outside = False
+        for succ in this_function_cfg.successors(guard):
+            if succ not in loop:
+                goes_outside = True
+                break
+        if goes_outside:
+            leave_loop.append(guard)
+
+    if len(leave_loop) == 1:
+        return leave_loop[0]
+
+    # not found
+    return None
+
+
+# calculate weight of each block (probability of execution ignoring loops)
+# with each branch having equal probability
+def get_block_weight(loop_free_cfg, entry_node):
+    assert len(list(nx.simple_cycles(loop_free_cfg))) == 0  # no cycles
+    result = {node: 0.0 for node in loop_free_cfg.nodes}
+    result[entry_node] = 1.0
+
+    to_visit = {entry_node}
+    to_add = set()  # to implement BFS
+    visited = set()
+
+    while len(to_visit) > 0:
+        node = to_visit.pop()
+        for pred in loop_free_cfg.predecessors(node):
+            if pred not in visited:
+                # not all incoming edges where visited
+                to_add.add(node)  # need to re-visit in next BFS wave
+                continue
+        visited.add(node)
+        if node in to_add:
+            to_add.remove(node)  # no endless recursion
+        num_successors = len(loop_free_cfg.succ[node])
+        for succ in loop_free_cfg.succ[node]:
+            to_add.add(succ)
+            # propagate probability
+            result[succ] += result[node] * (1.0 / num_successors)
+
+        if len(to_visit) == 0:
+            to_visit = to_add.copy()
+            to_add.clear()
+
+    return result
+
+
+# remove all loop back edges from cfg
+# iterative algorithm: remove the edge that is included in most loops, but only if all nodes are still reachable, until no more loops remain
+def remove_back_edges(this_function_cfg, entry_node):
+    result = this_function_cfg.copy()
+    reachable = {entry_node} | networkx.descendants(result,
+                                                    entry_node)
+    assert reachable == set(result.nodes)
+
+    loops = list(networkx.simple_cycles(result))
+    num_loops = len(loops)
+    while len(loops) > 0:
+        removal_candidates = {}
+        # collect removal candidates
+        for loop in loops:
+            for i in range(len(loop) - 1):
+                if (loop[i], loop[i + 1]) not in removal_candidates:
+                    removal_candidates[(loop[i], loop[i + 1])] = 0
+                removal_candidates[(loop[i], loop[i + 1])] += 1
+            # wrap-around
+            if (loop[-1], loop[0]) not in removal_candidates:
+                removal_candidates[(loop[-1], loop[0])] = 0
+            removal_candidates[(loop[-1], loop[0])] += 1
+        # sort by number of cycles the edge is part of
+        removal_candidates = dict(sorted(removal_candidates.items(), key=lambda item: item[1]))
+        # try removal
+        for edge, _ in removal_candidates.items():
+            result.remove_edge(edge[0], edge[1])
+            reachable = {entry_node} | networkx.descendants(result,
+                                                            entry_node)
+            if not reachable == set(result.nodes):
+                # removal partitioned graph
+                result.add_edge(edge[0], edge[1])
+            else:
+                # found edge to remove
+                break
+        # check for other loops
+        loops = list(networkx.simple_cycles(result))
+        assert len(loops) < num_loops
+        num_loops = len(loops)
+
+    reachable = {entry_node} | networkx.descendants(result, entry_node)
+    assert reachable == set(result.nodes)
+    assert len(list(networkx.simple_cycles(result))) == 0
+    return result
