81e2ef7176cd275591cf3928a26a6170a782e5a2
Tim Jammer <tim.jammer@tu-darmstadt.de>
2024-03-11 13:29:13 +0100

    respect_refresh_repos flag

--- AnalyzeModule/AnalysisModule/AnalysisManager.py
+++ AnalyzeModule/AnalysisModule/AnalysisManager.py
@@ -8,19 +8,19 @@ from binaryornot.check import is_binary
 
 def analyze_asm_repo_single_arg(args):
     try:
-        analyze_asm_repo(args[0], args[1], args[2], args[3], args[4], args[5], args[6])
+        analyze_asm_repo(args[0], args[1], args[2], args[3], args[4], args[5])
     except Exception:
         print('Analysis of ' + args[0] + ' threw an Exception!')
 
 
-def analyze_asm_repo(repo_name, repo_base_path, resultdir, ignore_endings, ignore_folders, refresh_repos, keep_data,
+def analyze_asm_repo(repo_name, repo_base_path, resultdir, ignore_endings, ignore_folders, print_cfg,
                      print_analyzed_files=False):
     outdir = os.path.join(resultdir, repo_name)
     os.makedirs(outdir, exist_ok=True)
 
     for root, dirs, files in os.walk(os.path.join(repo_base_path, repo_name)):
-        #https://stackoverflow.com/questions/19859840/excluding-directories-in-os-walk
-        #modifying dirs in-place will prune the (subsequent) files and directories visited by os.walk
+        # https://stackoverflow.com/questions/19859840/excluding-directories-in-os-walk
+        # modifying dirs in-place will prune the (subsequent) files and directories visited by os.walk
         dirs[:] = [d for d in dirs if d not in ignore_folders]
         for name in files:
             this_file = os.path.join(root, name)
@@ -46,27 +46,30 @@ def analyze_asm_repo(repo_name, repo_base_path, resultdir, ignore_endings, ignor
 
 class AnalysisManager:
     __slots__ = (
-        '_datadir', '_asmdir', '_resultdir', '_ignore_endings', '_ignore_folders', '_refresh_repos', '_keep_data')
+        '_datadir', '_asmdir', '_resultdir', '_ignore_endings', '_ignore_folders', '_refresh_repos', '_print_cfg')
 
-    def __init__(self, datadir, resultdir, ignore_endings, ignore_folders, refresh_repos=False, keep_data=True):
+    def __init__(self, datadir, resultdir, ignore_endings, ignore_folders, refresh_repos=False, print_cfg=False):
         assert os.path.isdir(datadir) and "The path where the repositories are lying must exist"
-        if (not os.path.isdir(resultdir)):
-            os.mkdir(resultdir)
+        os.makedirs(resultdir, exist_ok=True)
         self._datadir = datadir
         self._resultdir = resultdir
         self._ignore_endings = ignore_endings
         self._ignore_folders = ignore_folders
         self._refresh_repos = refresh_repos
-        self._keep_data = keep_data
+        self._print_cfg = print_cfg
 
     # perform the analyses
     def __call__(self, use_parallel_processing=True):
         with mp.Pool() as pool:
-            param_list = [(repo_dir, self._datadir, self._resultdir, self._ignore_endings, self._ignore_folders,
-                           self._refresh_repos,
-                           self._keep_data) for
-                          repo_dir in
-                          os.listdir(self._datadir)]
+            param_list = [
+                (repo_dir, self._datadir, self._resultdir, self._ignore_endings, self._ignore_folders, self._print_cfg)
+                for
+                repo_dir in
+                os.listdir(self._datadir)]
+
+            # filter out the repos where we have already collected some data
+            if not self._refresh_repos:
+                param_list = [p for p in param_list if not p[0] in os.listdir(self._resultdir)]
 
             if use_parallel_processing:
                 # parallel processing
